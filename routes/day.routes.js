f0621ac2da0f7919f73170380104b2058da6d27e
Habib Vadillo <habibvar@gmail.com>
2021-05-20 15:45:11 +0200

    days adjustments

--- routes/day.routes.js
+++ routes/day.routes.js
@@ -17,6 +17,7 @@ const isLoggedIn = (req, res, next) => {
 router.post("/day/start", isLoggedIn, (req, res, next) => {
   let { date, author } = req.body;
   DayModel.create({
+    meals: [],
     date,
     author,
   })
@@ -32,7 +33,39 @@ router.get("/days", isLoggedIn, (req, res, next) => {
   DayModel.find({ author: req.session.loggedInUser._id })
     .populate("meals")
     .then((result) => {
-      res.status(200).json(result);
+      let dayDec = result.length;
+      let today = new Date();
+      while (result.length < 7) {
+        let day = `${new Date(
+          new Date().setDate(today.getDate() - dayDec)
+        ).getDate()}`;
+        let month = `${
+          new Date(new Date().setDate(today.getDate() - dayDec)).getMonth() + 1
+        }`;
+        let year = `${new Date(
+          new Date().setDate(today.getDate() - dayDec)
+        ).getFullYear()}`;
+        result.unshift({
+          meals: [],
+          date: `${day}/${month}/${year}`,
+          author: req.session.loggedInUser._id,
+        });
+        DayModel.create({
+          meals: [],
+          date: `${day}/${month}/${year}`,
+          author: req.session.loggedInUser._id,
+        })
+          .then((result) => {
+            console.log("created day", result);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        dayDec++;
+      }
+      console.log(result.sort((a, b) => a.date.localeCompare(b.date)));
+
+      res.status(200).json(result.sort((a, b) => a.date.localeCompare(b.date)));
     })
     .catch((err) => {
       res.status(500).json(err);
